762367b275d991a693f5f124d910cfb9b88f2609
DESKTOP-K4V3L7P\MI NOTEBOOK <marcoscondori@gmail.com>
2025-10-11 14:03:06 -0300

    cambios

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom'
 import {useGetProducto} from '../../hooks/useGetProducto'
-
+import './ItemDetail.css'
 const ItemDetail = () =>{
     
     const {idProducto} = useParams()
@@ -11,52 +11,22 @@ const ItemDetail = () =>{
     
         return (
                <>
-                 <div
-                   className="modal fade"
-                   id={`exampleModal-${producto.id}`}
-                   tabIndex="-1"
-                   aria-labelledby={`exampleModalLabel-${producto.id}`}
-                   aria-hidden="true"
-                 >
-                   <div className="modal-dialog">
-                     <div className="modal-content">
-                       <div className="modal-header">
-                         <h5 className="modal-title fs-5" id={`exampleModalLabel-${producto.id}`}>
-                           {producto.nombre}
-                         </h5>
-                         <button
-                           type="button"
-                           className="btn-close"
-                           data-bs-dismiss="modal"
-                           aria-label="Close"
-                         ></button>
-                       </div>
-             
-                       <div className="modal-body">
-                         <img
-                           src={producto.imagen}
-                           alt={producto.nombre}
-                           title={producto.nombre}
-                           style={{ width: '100%', borderRadius: '8px' }}
-                         />
-                         <p>{producto.descripcion}</p>
-                         <h2>${producto.precio}</h2>
-                       </div>
-             
-                       <div className="modal-footer">
-                         <button
-                           type="button"
-                           className="btn btn-secondary"
-                           data-bs-dismiss="modal"
-                         >
-                           Cerrar
-                         </button>
-                       </div>
-                     </div>
+                <div className='producto-content'>
+                 
+                   <img src={producto.imagen} alt={producto.nombre} />
+                 
+                 <div className='producto-content-info'>
+                  <h1> {producto.nombre}</h1>
+                  <p>{producto.descripcion}</p>
+                  <h2> $ {producto.precio}</h2>
+                   <div className='botones'>
+                     <button className='btn btn-dark'>Agregar a carrito</button>
+                     <button className='btn btn-primary'>Comprar</button>
                    </div>
                  </div>
+                 </div>
                </>
-             );
+             )
 
           
 }
